9706f69e4f054c6eb917302b2455b872baf6df39
nasiqul <nasiqulibat@gmail.com>
2019-01-29 15:04:48 +0700

    change date to now rev3

--- application/models/absensi_model.php
+++ application/models/absensi_model.php
@@ -26,7 +26,7 @@ class Absensi_model extends CI_Model {
         $this->db->select('karyawan.nik, karyawan.namaKaryawan, presensi.tanggal, presensi.masuk, presensi.keluar, presensi.shift');
         $this->db->from('presensi');
         $this->db->join('karyawan','karyawan.nik = presensi.nik');
-        $this->db->where('date(presensi.tanggal) = CURRENT_DATE()');
+        $this->db->where('date(presensi.tanggal)','CURDATE()');
         $this->db->where('presensi.shift REGEXP','^[a-zA-Z]+$');
         $this->db->where('presensi.shift !=','OFF');
         $this->db->where('presensi.shift !=','X');
@@ -79,7 +79,7 @@ class Absensi_model extends CI_Model {
     }
 
     public function by_absensi(){
-        $q = "SELECT shift,tanggal, COUNT(*) AS jml from presensi where date(tanggal) = CURDATE() and shift <> 'off' AND shift <> 'X' and shift REGEXP '^[a-zA-Z]+$' group by shift order by jml ASC ";
+        $q = "SELECT shift,tanggal, COUNT(*) AS jml from presensi where date(tanggal) = CURRENT_DATE() and shift <> 'off' AND shift <> 'X' and shift REGEXP '^[a-zA-Z]+$' group by shift order by jml ASC ";
         $query = $this->db->query($q);
 
         if($query->num_rows() > 0){
