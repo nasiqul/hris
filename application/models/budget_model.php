f83e6a39578e798099982607bfb33bc5c870cd09
nasiqul <nasiqulibat@gmail.com>
2019-03-15 10:31:36 +0700

    form OT + sub-sec 3

--- application/models/budget_model.php
+++ application/models/budget_model.php
@@ -77,9 +77,9 @@ class Budget_model extends CI_Model {
     {
         $q = "select COUNT(C.NIK) as jumlah, c.costCenter, b.name, a.budget, a.aktual from karyawan as c
         LEFT JOIN cost_center_budget as a on a.id_cc = c.costCenter
-        LEFT JOIN master_cc as b on a.id_cc = b.id_cc
-        GROUP BY c.costCenter
-        where b.period = '".$tgl."'";
+        LEFT JOIN master_cc as b on a.id_cc = b.id_cc        
+        where a.period = '".$tgl."'
+        GROUP BY c.costCenter";
         $query = $this->db->query($q);
         return $query->result();
     }
