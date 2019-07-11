a834d9a09bd34003e8a95b803c463727886afb2c
nasiqul <nasiqulibat@gmail.com>
2019-07-11 09:53:05 +0700

    rev3

--- application/views/overtime_control_new.php
+++ application/views/overtime_control_new.php
@@ -216,7 +216,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           // seriesDataBudget.push(data[0][i][2]);
           seriesDataBudget.push(Math.round(data[0][i][2] * 100) / 100);
           seriesDataAktual.push(data[0][i][3]);
-          budgetHarian.push(Math.round(data[0][i][2] * 100) / 100);
+          budgetHarian.push(Math.round(data[0][i][5] * 100) / 100);
           if(xCategories.indexOf(cat) === -1){
            xCategories[xCategories.length] = cat;
          }
