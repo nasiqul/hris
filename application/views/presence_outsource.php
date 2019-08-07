ae86a140839ca760c213a78f26e032436036f635
nasiqul <nasiqulibat@gmail.com>
2019-08-07 13:29:12 +0700

    fixing

--- application/views/presence_outsource.php
+++ application/views/presence_outsource.php
@@ -132,7 +132,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title">Input Presensi Driver</h4>
+                    <h4 class="modal-title">Input Presensi Driver Outsource</h4>
                   </div>
                   <div class="modal-body">
                     <div class="row">
